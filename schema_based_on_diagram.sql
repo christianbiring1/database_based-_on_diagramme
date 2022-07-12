16d3813092980131588e83891a46e860b6744b63
yasin <yasin.elsharawi23@gmail.com>
2022-07-12 21:23:36 +0300

    Create invoices, invoice_items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE clinic;
+
 CREATE TABLE patients(id SERIAL PRIMARY KEY,
 name VARCHAR(100) NOT NULL,
 date_of_birth DATE NOT NULL);
-
 
 CREATE TABLE medical_histories(
   id SERIAL PRIMARY KEY,
@@ -13,3 +13,20 @@ CREATE TABLE medical_histories(
 CREATE TABLE treatments(id SERIAL PRIMARY KEY,
 type VARCHAR(100)NOT NULL,
 name VARCHAR(100) NOT NULL);
+
+CREATE TABLE invoices(
+	id SERIAL PRIMARY KEY, 
+	total_amount decimal, 
+	generated_at TIMESTAMP, 
+	payed_at TIMESTAMP, 
+	medical_history_id INT
+);
+
+CREATE TABLE invoice_items(
+	id SERIAL PRIMARY KEY, 
+	unit_price decimal, 
+	quantity INT, 
+	total_price DECIMAL, 
+	invoice_id INT, 
+	treatment_id INT
+);
