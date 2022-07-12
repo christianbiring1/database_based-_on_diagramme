de0949e310bc3a14d127fbf3e1794f82d0d568dd
christian <christianbiringanine22@gmail.com>
2022-07-12 19:46:05 +0200

    create patients, medical_histories and treatments tables

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE clinic;
+CREATE TABLE patients(id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+date_of_birth DATE NOT NULL);
+
+
+CREATE TABLE medical_histories(
+  id SERIAL PRIMARY KEY,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INTEGER NOT NULL,
+  status VARCHAR(500) NOT NULL);
+
+CREATE TABLE treatments(id SERIAL PRIMARY KEY,
+type VARCHAR(100)NOT NULL,
+name VARCHAR(100) NOT NULL);
